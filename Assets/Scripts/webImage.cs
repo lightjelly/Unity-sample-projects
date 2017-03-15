718825c9713782ba61403cd053384bfc498a3be9
JELLY-PC\JELLY <wongsuihong7@gmail.com>
2017-03-15 14:56:02 -0700

    -Finished 4th Scene

---
+++ Assets/Scripts/webImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webImage : MonoBehaviour {
+
+    [SerializeField] 
+    public string URL;
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW(URL); //Create new www object from URL
+        yield return www;
+
+        GetComponent<RawImage>().texture = www.texture; //Set texture of Ui object
+        GetComponent<RawImage>().SetNativeSize(); //optional, Set image to original size
+    }
+	
+}
